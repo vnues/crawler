28e791f4ffe7110ff5da7d21fb0fb166467e8ac1
wanggangfeng <wanggangfeng@forchange.tech>
2019-05-24 15:34:06 +0800

    添加engin和队列模块

--- engine/engine.go
+++ engine/engine.go
@@ -1,5 +1,84 @@
 package engine
 
+import ("fmt"
+	"log"
+
+	"../fetch"
+)
+
+func Run(seeds ...Request){
+	 fmt.Println("启动种子引擎")
+	 var requests []Request
+
+	 for _,r :=range seeds{
+	 	 requests=append(requests,r)
+	 }
+	 //将种子拿出队列执行
+	 for len(requests)>0{
+	 	  r :=requests[0]
+	 	  requests=requests[1:]
+	 	  //爬取页面
+		 //body html字符串--byte类型
+		  body,err :=fetch.Fetcher(r.Url)
+	 	  fmt.Printf("我正在爬取这个地址Fetching %s\n",r.Url)
+	 	  if err!=nil{
+	 	  	 //发生获取错误跳出这次循环
+	 	  	 log.Printf("Fetch error "+"fetching url %s:%v",r.Url,err)
+	 	  	 continue
+		  }
+	 	  //解析
+	 	  ParseResult :=r.ParseFunc(body)
+	 	  //解析出来的结果里面可能包含种子，需要把种子放进去队列
+	 	  //append另外一种形式...表示拼接
+	 	  requests =append(requests,ParseResult.Requests...)
+	 	  for _,item :=range ParseResult.Items{
+	 	  	fmt.Printf("爬取所得到到信息：%v\n",item)
+		  }
+
+	 }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //go append方式是slice方法 有两种形式追加和拼接(就是打散切片)
 
 //engine这快是把爬取的数据打印出来并且又把种子放进去队列执行
@@ -26,3 +105,5 @@ var strss= []string{
     test1(strss...) //切片被打散传入
 }
 */
+
+
